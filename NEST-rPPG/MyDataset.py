04df3beaf0c261c5eeb0d8d936d7fee7515ae317
Yihang Jiang <yj167@wil.egr.duke.edu>
2026-02-04 10:53:40 -0500

    test

--- NEST-rPPG/MyDataset.py
+++ NEST-rPPG/MyDataset.py
@@ -13,6 +13,7 @@ from torch.autograd import Variable
 import random
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
+import math
 
 
 class Data_DG(Dataset):
